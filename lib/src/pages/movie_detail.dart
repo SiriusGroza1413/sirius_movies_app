1922b35d716916db29826b8df30f44ef503b854c
Guillermo Ojeda <gro1413@hotmail.com>
2023-08-17 19:01:03 -0300

    minor: delete import

--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:sirius_movies_app/src/models/movie_model.dart';
-import 'package:sirius_movies_app/src/providers/movies_provider.dart';
 
 
 class MovieDetailPage extends StatelessWidget {
